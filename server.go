bed8b9fba37a02e620734049249e66a060ae95bc
zhiyang <zhiyang.deng@aliyun.com>
2018-01-17 12:09:55 +0800

    编写简单的文件服务器，做预览用

---
+++ server.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("build"))
+	http.Handle("/", fs)
+	log.Println("Listening...")
+	http.ListenAndServe(":5001", nil)
+}
